607c6ff57b1f77ee0271d6e7c239aaff42d6d24d
javid shabin <javid.main.act@gmail.com>
2024-08-18 13:18:52 +0530

    Install cooki parser and cors the config add expres.json middleware also

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ require('dotenv').config();
 const express = require("express");
 const { apiRouter } = require('./routes');
 const app = express();
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(cookieParser())
+app.use(express.json())
 
 // Middleware to handle requests
 app.get('/', (req, res) => {
