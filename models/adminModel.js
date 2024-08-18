583866317ce5866fd189eef29cd7b0bcda14d32c
javid shabin <javid.main.act@gmail.com>
2024-08-18 14:42:31 +0530

    Create admin model

---
+++ models/adminModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const adminSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  phone: { type: String, required: true },
+  image: {
+    type: String,
+    default:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTL_JlCFnIGX5omgjEjgV9F3sBRq14eTERK9w&s",
+  },
+  role: { type: String, enum: ["customer", "admin"], default: "customer" },
+});
+
+const Admin = mongoose.model("admin", adminSchema);
+module.exports = { Admin };
