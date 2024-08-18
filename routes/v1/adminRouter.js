40d53bc05d216312cd287544637e01c68a558b0b
javid shabin <javid.main.act@gmail.com>
2024-08-18 15:38:47 +0530

    Create admin controller and added controolers body for admin and call in admin router

--- routes/v1/adminRouter.js
+++ routes/v1/adminRouter.js
@@ -1,11 +1,19 @@
 const express = require("express");
+const {
+  registerAdmin,
+  loginAdmin,
+  logoutAdmin,
+  checkAdmin,
+  getProfile,
+  updateProfile,
+} = require("../../controllers/adminController");
 const router = express();
 
-router.post("/register");
-router.post("/login");
-router.post("/logout");
-router.get("/profile");
-router.put("/update");
-router.get("/check-admin");
+router.post("/register", registerAdmin);
+router.post("/login", loginAdmin);
+router.post("/logout", logoutAdmin);
+router.get("/profile", getProfile);
+router.put("/update", updateProfile);
+router.get("/check-admin", checkAdmin);
 
 module.exports = { adminRouter: router };
