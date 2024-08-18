d513656c52d70cae91cedf4f6d66553d3e772a43
javid shabin <javid.main.act@gmail.com>
2024-08-18 15:39:18 +0530

    Create admin controller and added controolers body for admin and call in admin router

---
+++ controllers/adminController.js
@@ -0,0 +1,38 @@
+const registerAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const loginAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const logoutAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const getProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const updateProfile = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+const checkAdmin = async (req, res) => {
+  try {
+  } catch (error) {}
+};
+
+module.exports = {
+  registerAdmin,
+  loginAdmin,
+  logoutAdmin,
+  getProfile,
+  updateProfile,
+  checkAdmin,
+};
