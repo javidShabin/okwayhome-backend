c287317b880e5c0bd1a2a0c8f7046d58a7fe9b02
javid shabin <javid.main.act@gmail.com>
2024-08-18 15:51:36 +0530

    Created admin register function

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,54 @@
+const { Admin } = require("../models/adminModel");
+const bcrypt = require("bcrypt");
+const { generateToken } = require("../utils/token");
+
 const registerAdmin = async (req, res) => {
   try {
-  } catch (error) {}
+    // Get datas from req.body
+    const { email, password, conformPassword, ...rest } = req.body;
+    // Check if rerquired fields are present
+    if (!email || !password || !conformPassword) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    // Check if password and confirm password match
+    if (password !== conformPassword) {
+      return res.status(400).json({ message: "Passwords do not match" });
+    }
+    // Check if the admin already exists
+    const isAdminExist = await Admin.findOne({ email });
+    if (isAdminExist) {
+      return res.status(409).json({ message: "User already exists" });
+    }
+    // Hash the password using bcrypt
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    // Create a new admin and save the to databse
+    const newAdmin = new Admin({ email, ...rest, password: hashedPassword });
+    await newAdmin.save();
+
+    // Generate a token
+    const token = generateToken({
+      _id: newAdmin._id,
+      email: newAdmin.email,
+      role: "customer",
+    });
+    // Pass the token as cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production", // Set secure to true in production
+      maxAge: 3600000, // 1 hour
+    });
+    // Return a success response
+    res.status(201).json({
+      success: true,
+      message: "Admin created successfully",
+    });
+  } catch (error) {
+    console.error(error); // Log the error for debugging
+    res
+      .status(500)
+      .json({ message: "User creation failed", error: error.message });
+  }
 };
 
 const loginAdmin = async (req, res) => {
