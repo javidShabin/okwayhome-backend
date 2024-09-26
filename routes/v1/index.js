0cf7ca384d622fef140c8e86807af353d3fb0b95
javid shabin <javid.main.act@gmail.com>
2024-09-26 22:51:33 +0530

    Create address router and add neede routes

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -3,11 +3,13 @@ const { userRouter } = require("./userRouter");
 const { adminRouter } = require("./adminRouter");
 const { productRouter } = require("./productRouter");
 const { cartRouter } = require("./cartRouter");
+const { addressRouter } = require("./addressRouter");
 const router = express.Router();
 
 router.use('/user', userRouter)
 router.use('/admin', adminRouter)
 router.use('/product', productRouter)
 router.use('/cart', cartRouter)
+router.use('/address', addressRouter)
 
 module.exports = { v1Router: router };
