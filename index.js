5998927485957e11ce92a6f50b50f134d51c493b
javid shabin <javid.main.act@gmail.com>
2024-10-08 01:09:36 +0530

    corse creadantial and orgin aded

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { dbConnection } = require("./config/dbConnection");
-const port = process.env.PORT || 5000;
+const port = 5000;
 
 app.use(
   cors({
