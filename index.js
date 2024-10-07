da666600c6b9f2a46816f78ab702a1a502f4f18d
javid shabin <javid.main.act@gmail.com>
2024-10-07 21:22:00 +0530

    Add creadential and orging

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const { apiRouter } = require('./routes');
+const { apiRouter } = require("./routes");
 const app = express();
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
-const { dbConnection } = require('./config/dbConnection');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const { dbConnection } = require("./config/dbConnection");
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(cookieParser())
-app.use(express.json())
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
 // Middleware to handle requests
-app.get('/', (req, res) => {
-    res.send("Hello world");
+app.get("/", (req, res) => {
+  res.send("Hello world");
 });
 
-app.use('/api', apiRouter)
+app.use("/api", apiRouter);
 
-dbConnection()
+dbConnection();
 
 // Start the server
 app.listen(port, () => {
-    console.log(`The server is running on port ${port}`);
+  console.log(`The server is running on port ${port}`);
 });
