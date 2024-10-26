55ee632ace9f3c22a06b87f6641097dbb7032d71
javid shabin <javid.main.act@gmail.com>
2024-10-26 22:47:57 +0530

    Create and config address model and schema for geting user address

---
+++ models/addressModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const addressSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  phone: { type: String, required: true },
+  district: { type: String, require: true },
+  street: { type: String, required: true },
+  landmark: { type: String, required: true },
+  postalCode: { type: String, required: true },
+});
+const Address = mongoose.model("Address", addressSchema);
+module.exports = { Address };
